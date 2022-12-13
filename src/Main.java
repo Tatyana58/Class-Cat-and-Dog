da836fb18497b315c397d30703ef9b0185752643
Tatyana58 <tatbir@rambler.ru>
2022-12-13 21:34:41 +0300

    Кот и пес считаем вес в методе Обявили двух животных и считаем сколько съел

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Классы Кот и Собака");
-        Pet pet = new Pet();
+        Pet cat = new Pet();
         /*pet.type = "Кот";
         pet.name = "Барсик";
         pet.weight = 10;
@@ -25,6 +25,45 @@ public class Main {
     }
 
          */
-        pet.eat(1).eat(1).run();
+        //pet.eat(1).eat(1).run();
+        cat.type = "Кот";
+        cat.name = "Барсик";
+        cat.weight = 10;
+        System.out.println(
+                "Домашнее животное: " + cat.type +
+                        "\nКличка: " + cat.name +
+                        "\nВес: " + cat.weight +
+                        "\nСколько животное съело: " + cat.amountOfFood);
+
+        Pet dog = new Pet();
+        dog.type = "Собака";
+        dog.name = "Тузик";
+        dog.weight = 15;
+
+        System.out.println(
+                "Домашнее животное: " + dog.type +
+                        "\nКличка: " + dog.name +
+                        "\nВес: " + dog.weight +
+                        "\nСколько животное съело: " + dog.amountOfFood);
+
+        System.out.println("Общее количество съеденной еды: " + Pet.amountOfAllFood);
+
+        cat.eat(4);
+        dog.eat(6);
+
+        System.out.println(
+                "Домашнее животное: " + cat.type +
+                        "\nКличка: " + cat.name +
+                        "\nВес: " + cat.weight +
+                        "\nСколько животное съело: " + cat.amountOfFood);
+
+        System.out.println(
+                "Домашнее животное: " + dog.type +
+                        "\nКличка: " + dog.name +
+                        "\nВес: " + dog.weight +
+                        "\nСколько животное съело: " + dog.amountOfFood);
+
+        System.out.println("Общее количество съеденной еды: " + Pet.amountOfAllFood);
+
     }
 }
